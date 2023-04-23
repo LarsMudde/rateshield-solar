51537321316b5c0a4ce3a156d71a09e16fe28779
LarsMudde <Lars>
2023-04-23 20:13:40 +0200

    Update forecast at startup if needed

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,34 @@
 #include <ezTime.h>
 #include "WebServer.h"
 #include "Constants.h"
+#include "DataHandler.h"
 
 AsyncWebServer server(80);
 AsyncDNSServer dnsServer;
 
 ESPAsync_WiFiManager* wifiManager;
+
+const unsigned long MAX_FILE_AGE = 8 * 60 * 60 * 1000UL;  // 8 hours in milliseconds
+
+bool shouldFetchForecast() {
+  File file = SPIFFS.open(FORECAST_FILE_NAME, "r");
+  if (!file) {
+    // File doesn't exist
+    Serial.println("forecast.json doesn't exist");
+    return true;
+  }
+  unsigned long lastModified = file.getLastWrite();
+  file.close();
+  unsigned long age = millis() - lastModified;
+  if (age >= MAX_FILE_AGE) {
+    // File is too old
+    Serial.printf("forecast.json is %lu ms old, fetching new data\n", age);
+    return true;
+  }
+  // File is up to date
+  Serial.printf("forecast.json is %lu ms old, not fetching new data\n", age);
+  return false;
+}
 
 void setupFileSystem() {
   if (!SPIFFS.begin(true)) {
@@ -27,16 +50,15 @@ void setupWiFi() {
 }
 
 void setupTime() {
-  waitForSync(); // Remove the UTC.begin() line, keep waitForSync()
+  waitForSync();
   Serial.println("UTC: " + UTC.dateTime());
 
   Timezone myTZ;
-  myTZ.setLocation(F(TIMEZONE)); // Set timezone to Amsterdam
+  myTZ.setLocation(F(TIMEZONE));
   Serial.println("Local time: " + myTZ.dateTime());
 }
 
 void setupWebServer() {
-  // Only setup the web server if the Wi-Fi connection is successful
   if (WiFi.status() == WL_CONNECTED) {
     Serial.print("Connected to WiFi! IP address: ");
     Serial.println(WiFi.localIP());
@@ -49,10 +71,15 @@ void setupWebServer() {
   }
 }
 
-unsigned long lastSyncTime = 0;
-const unsigned long oneWeek = 7 * 24 * 60 * 60 * 1000UL;
+void getForecast() {
+  Serial.println("Fetching new forecast...");
+  getFromEndpoint();
+}
 
 void syncTimeEveryWeek() {
+  static unsigned long lastSyncTime = 0;
+  const unsigned long oneWeek = 7 * 24 * 60 * 60 * 1000UL;
+  
   if (millis() - lastSyncTime >= oneWeek) {
     waitForSync();
     lastSyncTime = millis();
@@ -65,6 +92,11 @@ void setup() {
 
   setupFileSystem();
   setupWiFi();
+
+  if (shouldFetchForecast()) {
+    getForecast();
+  }
+
   setupWebServer();
 }
 
